320673c050ef0bc968380f82f3b45dfc75412345
Myles Byrne <myles@myles.id.au>
2013-10-14 15:35:44 -0700

    Initial commit - Rails (4.0.0) project boilerplate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chatbot::Application.config.secret_key_base = '7b60c277aba4b6251047f5894abd90d3dd8999bd535551c40fb151f9fc07f40213ad515c9ca7675066d018de51a83d3487b672df98197ce4034c277f249684a9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Chatbot::Application.config.session_store :cookie_store, key: '_chatbot_session'
